a24e8b5aa86786d592e6d509049185fd86008231
happy-yuxuan <victor.wuy@icloud.com>
2019-03-07 17:06:48 +0800

    在月球旁边添加一个精灵球

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -20,11 +20,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // 增加这个防止东西是平面，增加一句可以增加物体的3D效果
+        sceneView.autoenablesDefaultLighting = true
+        
+        // MARK: 月亮模型制作到部署
+        
         // 自己创建的模型
 //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)    //通过SCNBox创建一个正方体的形状，这个正方体是个类型，是SCNNode.geometry的属性，到时候可以作用到SCNNode上，这个Node就是正方形的了
-        let sphere = SCNSphere(radius: 0.2)
+        // 创建月亮
+        let sphere = SCNSphere(radius: 0.2) // 创建一个圆， 角度为0.2
         
-        let material = SCNMaterial()
+        let material = SCNMaterial()    // 这个是用来组成圆的材料
         // download at https://www.solarsystemscope.com/textures/   MOON
         material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
         
@@ -38,10 +44,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.scene.rootNode.addChildNode(node) // 屏幕是一个sceneView中的scene的rootNode中增加一个节点在屏幕上
         
-        // 增加这个防止东西是平面，增加一句可以增加物体的3D效果
-        sceneView.autoenablesDefaultLighting = true
+        // MARK: 精灵球模型制作到部署
         
+        //  精灵球
+        let pokeballscene = SCNScene(named: "art.scnassets/Pokeball.scn")!  // !表示一定可以wrap
         
+        if let pokeball = pokeballscene.rootNode.childNode(withName: "Scene_Root", recursively: true) {
+            pokeball.position = SCNVector3(x: 0.5, y: 0, z: -0.1)
+            sceneView.scene.rootNode.addChildNode(pokeball)
+        }
 //        // Create a new scene（系统自带的飞机模型）
 //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
 //
